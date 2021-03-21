75ee72472a6ca3987b1d214b037da9f552228c1f
Oksana Zhekalova <oks.gek@gmail.com>
2021-03-21 13:39:14 +0300

    fixed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -10,11 +10,15 @@ for (let link of menuLinks) {
     let subMenu = link.closest(".menu__item").getElementsByClassName("menu_sub");
     subMenu = (subMenu.length) ? subMenu[0] : null;
     if (subMenu !== null) {
-        link.href = "javascript:void(0)";
-        link.onclick = () => {
-            hideAllSubmenus();
-            subMenu.classList.add("menu_active");
+        link.onclick = (evt) => {
+            evt.preventDefault();
             
+            if (subMenu.classList.contains("menu_active")) {
+                subMenu.classList.remove("menu_active");
+            } else {
+                hideAllSubmenus();
+                subMenu.classList.add("menu_active");
+            }
         }
     }
 }
