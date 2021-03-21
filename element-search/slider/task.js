87cc3e0d8e9c9e76570c3baf9bcf4e3481839f40
Oksana Zhekalova <oks.gek@gmail.com>
2021-03-21 13:40:14 +0300

    fixed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,46 +1,56 @@
 'use strict'
 
-function sliderNext() {
-    let currentSlide = document.getElementsByClassName("slider__item_active")[0];
-    currentSlide.classList.remove("slider__item_active");
-    if (currentSlide.nextElementSibling !== null) {
-        currentSlide.nextElementSibling.classList.add("slider__item_active");
-    } else {
-        let slides = document.getElementsByClassName("slider__item");
-        slides[0].classList.add("slider__item_active");
-    }
-}
+const slidesCount = document.getElementsByClassName("slider__item").length;
 
-function sliderPrev() {
-    let currentSlide = document.getElementsByClassName("slider__item_active")[0];
-    currentSlide.classList.remove("slider__item_active");
-    if (currentSlide.previousElementSibling !== null) {
-        currentSlide.previousElementSibling.classList.add("slider__item_active");
-    } else {
-        let slides = document.getElementsByClassName("slider__item");
-        slides[slides.length - 1].classList.add("slider__item_active");
-    }
-}
+function selectItem(index) {
+    let dots = document.getElementsByClassName('slider__dot');
+    let slides = document.getElementsByClassName("slider__item");
 
-function selectSliderDot(dot) {
-    let dots = Array.prototype.slice.call(document.getElementsByClassName('slider__dot'));
-    let index = dots.indexOf(dot);
-    
+    let activeSlides = document.getElementsByClassName("slider__item_active");
+    if (activeSlides.length) {
+        activeSlides[0].classList.remove("slider__item_active");
+    }
     let activeDots = document.getElementsByClassName("slider__dot_active");
     if (activeDots.length) {
         activeDots[0].classList.remove("slider__dot_active");
     }
-    document.getElementsByClassName("slider__item_active")[0].classList.remove("slider__item_active");
 
-    let slides = document.getElementsByClassName("slider__item");
     slides[index].classList.add("slider__item_active");
-    dot.classList.add("slider__dot_active");
+    dots[index].classList.add("slider__dot_active");
 }
+
+function getCurrentSlideIndex() {
+    let currentSlide = document.getElementsByClassName("slider__item_active")[0];
+    let slides = Array.prototype.slice.call(document.getElementsByClassName('slider__item'));
+    return slides.indexOf(currentSlide);
+}
+
+function sliderNext() {
+    let index = getCurrentSlideIndex();
+
+    if (index < slidesCount - 1) {
+        selectItem(index + 1);
+    } else {
+        selectItem(0);
+    }
+}
+
+function sliderPrev() {
+    let index = getCurrentSlideIndex();
+
+    if (index > 0) {
+        selectItem(index - 1);
+    } else {
+        selectItem(slidesCount - 1);
+    }
+}
+
+selectItem(0);
 
 document.getElementsByClassName("slider__arrow_next")[0].onclick = () => sliderNext();
 document.getElementsByClassName("slider__arrow_prev")[0].onclick = () => sliderPrev();
 
 let dots = document.getElementsByClassName("slider__dot");
-for (let dot of dots) {
-    dot.onclick = () => selectSliderDot(dot);
+for (let index = 0; index < dots.length; index++) {
+    dots[index].onclick = () => selectItem(index);
 }
