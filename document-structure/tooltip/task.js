37052dcf745721ec7eb76a12916645fbaf9c2a92
Oksana Zhekalova <oks.gek@gmail.com>
2021-04-08 12:32:01 +0300

    fix tooltips

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,18 +1,24 @@
 'use strict'
 
 const links = document.querySelectorAll('.has-tooltip');
-const tooltip = document.createElement('div');
-tooltip.className = 'tooltip';
-tooltip.style.display = 'block';
+//const tooltip = document.createElement('div');
+//tooltip.className = 'tooltip';
+//tooltip.style.display = 'block';
 
 [...links].forEach((link) => {
+    link.insertAdjacentHTML('afterEnd', `<div class = 'tooltip' style = ''> ${link.getAttribute('title')} </div>`);
     link.addEventListener('click', (e) => {
         e.preventDefault();
-        if (link.nextSibling.className === 'tooltip') {
+        /*if (link.nextSibling.className === 'tooltip') {
             link.nextSibling.remove(); 
         } else {
             tooltip.textContent = link.title;
             link.after(tooltip);
-        };
+        };*/
+        let tooltip = link.nextSibling;
+        let positionTooltip = link.getBoundingClientRect();
+        tooltip.style.left = positionTooltip.left + 'px';
+        tooltip.style.top = positionTooltip.bottom + 'px';
+        tooltip.classList.toggle('tooltip_active');
     })
 });
